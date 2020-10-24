90ee585dbfeefd7307506881371ed07fa1bfe155
Erroler <fabio_the_boss@hotmail.com>
2020-10-24 17:45:21 +0100

    Changed color palette

--- app/src/main/java/com/feup/cmov/acme_client/MainActivity.kt
+++ app/src/main/java/com/feup/cmov/acme_client/MainActivity.kt
@@ -3,6 +3,7 @@ package com.feup.cmov.acme_client
 import androidx.databinding.DataBindingUtil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.feup.cmov.acme_client.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
