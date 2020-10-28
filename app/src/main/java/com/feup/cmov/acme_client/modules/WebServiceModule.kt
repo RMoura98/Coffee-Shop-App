898baf04940bd55593549c600942198610edf607
Erroler <fabio_the_boss@hotmail.com>
2020-10-28 16:30:27 +0000

    Added timestamp for authentication.

--- app/src/main/java/com/feup/cmov/acme_client/modules/WebServiceModule.kt
+++ app/src/main/java/com/feup/cmov/acme_client/modules/WebServiceModule.kt
@@ -1,7 +1,6 @@
 package com.feup.cmov.acme_client.modules
 
 import android.util.Base64
-import android.util.Base64.encodeToString
 import android.util.Log
 import com.feup.cmov.acme_client.AcmeApplication
 import com.feup.cmov.acme_client.R
@@ -12,10 +11,15 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
-import okhttp3.*
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import retrofit2.Invocation
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Singleton
 
 
@@ -43,6 +47,16 @@ object WebServiceModule {
 
 
     class AuthenticatedRequestInterceptor : Interceptor {
+
+        fun getDateHeader(): String {
+            val calendar = Calendar.getInstance()
+            val dateFormat = SimpleDateFormat(
+                "EEE, dd MMM yyyy HH:mm:ss z", Locale.US
+            )
+            dateFormat.timeZone = TimeZone.getTimeZone("GMT")
+            return dateFormat.format(calendar.time)
+        }
+
         override fun intercept(chain: Interceptor.Chain): Response {
 
             val request: Request = chain.request()
@@ -53,26 +67,25 @@ object WebServiceModule {
                 if (userName == null || uuid == null) return chain.proceed(request)
                 val key = Security.retrieveRsaPrivateKey(userName)
 
-                val signature: String
-                if(request.method() == "GET" || request.method() == "DELETE") {
-                    val toSign: String
-                    if (request.url().url().query == null)
-                        toSign = request.url().url().path
-                    else
-                        toSign = request.url().url().path + "?" + request.url().url().query
-                    val buffer = okio.Buffer()
-                    buffer.write(toSign.toByteArray())
-                    signature = Security.makeSignature(key, buffer)
-                }
-                else { // POST, etc
-                    val requestBody = request.body()?: return chain.proceed(request)
-                    val buffer = okio.Buffer()
-                    requestBody.writeTo(buffer)
-                    signature = Security.makeSignature(key, buffer)
-                }
+                val dateHeader = getDateHeader()
+                val url: String
+                if (request.url().url().query == null)
+                    url = request.url().url().path
+                else
+                    url = request.url().url().path + "?" + request.url().url().query
+
+                val buffer = okio.Buffer()
+                request.body()?.writeTo(buffer)
+                buffer.write(",".toByteArray())
+                buffer.write(url.toByteArray())
+                buffer.write(",".toByteArray())
+                buffer.write(dateHeader.toByteArray())
+
+                val signature = Security.makeSignature(key, buffer)
                 val authHeader = "$uuid,$signature"
                 val newRequest = request.newBuilder()
                     .addHeader("Authorization", authHeader)
+                    .addHeader("Date", dateHeader)
                     .build()
                 return chain.proceed(newRequest)
             }
