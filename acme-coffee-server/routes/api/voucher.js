5732c72f6561ef2487f447fc7f19861af1e0c002
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-11-02 11:26:32 +0000

    Add authentication to vouchers request

--- acme-coffee-server/routes/api/voucher.js
+++ acme-coffee-server/routes/api/voucher.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('../../controllers/voucherController');
+const authentication = require('../../middleware/authentication');
 
-router.get('/:user/unused', controller.getUnusedVouchers);
+router.get('/:user/unused', authentication, controller.getUnusedVouchers);
 
 module.exports = router;
