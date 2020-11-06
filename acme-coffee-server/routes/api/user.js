a9f393ef0e485ab0fee867c0bb2914fd5c89e29d
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-11-06 11:25:57 +0000

    Add user update

--- acme-coffee-server/routes/api/user.js
+++ acme-coffee-server/routes/api/user.js
@@ -3,5 +3,6 @@ const controller = require('../../controllers/userController');
 
 router.get('/:user', controller.getUser);
 router.post('/new', controller.createUser);
+router.put('/:user', controller.updateUser);
 
 module.exports = router;
