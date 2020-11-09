bd0c13307867f7b68c766bbe8ed977e820174faf
Erroler <fabio_the_boss@hotmail.com>
2020-11-09 01:02:30 +0000

    Fixed bug regarding logging in after logging out.

--- app/src/main/java/com/feup/cmov/acme_client/screens/settings/SettingsFragment.kt
+++ app/src/main/java/com/feup/cmov/acme_client/screens/settings/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.feup.cmov.acme_client.screens.settings
 
+import android.content.Intent.getIntent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,16 +8,13 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import com.feup.cmov.acme_client.MainActivity
 import com.feup.cmov.acme_client.R
-import com.feup.cmov.acme_client.databinding.FragmentProfileBinding
 import com.feup.cmov.acme_client.databinding.FragmentSettingsBinding
-import com.feup.cmov.acme_client.databinding.FragmentSignupBinding
 import com.feup.cmov.acme_client.utils.PreferencesUtils
-import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
-import java.lang.IllegalArgumentException
+
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment(), SettingsHandler {
@@ -59,8 +57,9 @@ class SettingsFragment : Fragment(), SettingsHandler {
 
     override fun onLogoutButtonClick(v: View) {
         PreferencesUtils.logoutUser()
-        v.findNavController()
-            .navigate(R.id.action_mainMenuFragment_to_loginFragment)
+        val intent = MainActivity.getActivity().intent
+        MainActivity.getActivity().finish()
+        startActivity(intent)
     }
 
 
