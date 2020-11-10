2ce08b512023d3795d32c99f15255b71e7216dd8
Erroler <fabio_the_boss@hotmail.com>
2020-11-10 11:41:21 +0000

    Added logo to QR code.

--- app/src/main/java/com/feup/cmov/acme_client/screens/orders/pickup_order/OrderPickupFragment.kt
+++ app/src/main/java/com/feup/cmov/acme_client/screens/orders/pickup_order/OrderPickupFragment.kt
@@ -1,8 +1,7 @@
 package com.feup.cmov.acme_client.screens.orders.pickup_order
 
-import android.graphics.Bitmap
+import android.graphics.*
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +14,7 @@ import com.feup.cmov.acme_client.comm.Packet
 import com.feup.cmov.acme_client.database.models.composed_models.OrderWithItems
 import com.feup.cmov.acme_client.databinding.FragmentOrderPickupBinding
 import com.feup.cmov.acme_client.utils.Measurements
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -42,8 +42,14 @@ class OrderPickupFragment : Fragment() {
         val packet = Packet(orderWithItems)
 
         val qrCode: Bitmap =
-            QRCode.from("${packet.signature},${packet.payloadString}").withSize(Measurements.convertDptoPx(360).toInt(), Measurements.convertDptoPx(360).toInt()).bitmap()
-        binding.qrCode.setImageBitmap(qrCode)
+            QRCode.from("${packet.signature},${packet.payloadString}").withSize(
+                Measurements.convertDptoPx(360).toInt(),
+                Measurements.convertDptoPx(360).toInt()
+            ).withErrorCorrection(
+                ErrorCorrectionLevel.Q
+            ).bitmap()
+        val merge = mergeBitmaps(BitmapFactory.decodeResource(resources, R.drawable.coffee_trimmed), qrCode)
+        binding.qrCode.setImageBitmap(merge)
 
         GlobalScope.launch {
             delay(75000)
@@ -54,10 +60,45 @@ class OrderPickupFragment : Fragment() {
         }
 
         binding.topAppBar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
-        binding.topAppBar.setNavigationOnClickListener{
+        binding.topAppBar.setNavigationOnClickListener {
             activity?.onBackPressed();
         }
 
         return binding.root
+    }
+
+    companion object {
+        fun mergeBitmaps(logo: Bitmap?, qrcode: Bitmap): Bitmap? {
+            val combined =
+                Bitmap.createBitmap(qrcode.width, qrcode.height, qrcode.config)
+            val canvas = Canvas(combined)
+            val canvasWidth: Int = canvas.getWidth()
+            val canvasHeight: Int = canvas.getHeight()
+            canvas.drawBitmap(qrcode, Matrix(), null)
+            val resizeLogo =
+                Bitmap.createScaledBitmap(logo!!, canvasWidth / 6, canvasHeight / 6, true)
+            val centreX = (canvasWidth - resizeLogo.width) / 2
+            val centreY = (canvasHeight - resizeLogo.height) / 2
+            val center = Matrix()
+            center.setTranslate(centreX.toFloat(), centreY.toFloat())
+            canvas.drawBitmap(toGrayscale(resizeLogo), center, null)
+            return combined
+        }
+
+        fun toGrayscale(srcImage: Bitmap): Bitmap {
+            val bmpGrayscale = Bitmap.createBitmap(
+                srcImage.width,
+                srcImage.height,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bmpGrayscale)
+            val paint = Paint()
+            val cm = ColorMatrix()
+            cm.setSaturation(0f)
+            paint.colorFilter = ColorMatrixColorFilter(cm)
+            canvas.drawBitmap(srcImage, Matrix(), paint)
+            return bmpGrayscale
+        }
+
     }
 }
