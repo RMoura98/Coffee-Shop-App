1efdd1cf81fbefd3890a06d22d934ead9b676835
Erroler <fabio_the_boss@hotmail.com>
2020-11-10 22:43:00 +0000

    Removed NFC receiving from client

--- app/src/main/java/com/feup/cmov/acme_client/MainActivity.kt
+++ app/src/main/java/com/feup/cmov/acme_client/MainActivity.kt
@@ -29,7 +29,6 @@ class MainActivity : AppCompatActivity() {
     // TODO: REMOVE THIS BEFORE TURNING IN PROJECT!
     @Inject
     lateinit var debug: Debug
-    private var nfcAdapter: NfcAdapter? = NfcAdapter.getDefaultAdapter(AcmeApplication.getAppContext())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,67 +53,6 @@ class MainActivity : AppCompatActivity() {
         catch(e: Exception) {
             Log.e("MainActivity", e.toString())
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        enableForegroundDispatch(this, this.nfcAdapter)
-        receiveMessageFromDevice(intent)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        disableForegroundDispatch(this, this.nfcAdapter)
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        setIntent(intent)
-        //receiveMessageFromDevice(intent)
-    }
-
-    private fun receiveMessageFromDevice(intent: Intent) {
-        val action = intent.action
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
-            val parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)!!
-            with(parcelables) {
-                val inNdefMessage = this[0] as NdefMessage
-                val inNdefRecords = inNdefMessage.records
-                val ndefRecord_0 = inNdefRecords[0]
-
-                val inMessage = String(ndefRecord_0.payload)
-                ShowFeedback.makeSnackbar(inMessage)
-            }
-        }
-    }
-
-    private fun enableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
-
-        val intent = Intent(activity.applicationContext, activity.javaClass)
-        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-
-        val pendingIntent = PendingIntent.getActivity(activity.applicationContext, 0, intent, 0)
-
-        val filters = arrayOfNulls<IntentFilter>(1)
-        val techList = arrayOf<Array<String>>()
-
-        filters[0] = IntentFilter()
-        with(filters[0]) {
-            this?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
-            this?.addCategory(Intent.CATEGORY_DEFAULT)
-            try {
-                this?.addDataType("text/plain")
-            } catch (ex: IntentFilter.MalformedMimeTypeException) {
-                throw RuntimeException("Check your MIME type")
-            }
-        }
-
-        adapter?.enableForegroundDispatch(activity, pendingIntent, filters, techList)
-    }
-
-    private fun disableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
-        adapter?.disableForegroundDispatch(activity)
     }
 
     companion object {
