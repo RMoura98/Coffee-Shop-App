1c707ac8306318e6409972790ceafdcd2a1d2f00
Erroler <fabio_the_boss@hotmail.com>
2020-11-11 18:44:32 +0000

    ;;

--- app/src/main/java/com/feup/cmov/acme_terminal/MainActivity.kt
+++ app/src/main/java/com/feup/cmov/acme_terminal/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.feup.cmov.acme_terminal
 
 import android.app.Activity
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import com.feup.cmov.acme_terminal.databinding.ActivityMainBinding
@@ -11,11 +17,74 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private var nfcAdapter: NfcAdapter? = NfcAdapter.getDefaultAdapter(AcmeApplication.getAppContext())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         activity = this
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        enableForegroundDispatch(this, this.nfcAdapter)
+        receiveMessageFromDevice(intent)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disableForegroundDispatch(this, this.nfcAdapter)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        //setIntent(intent)
+        receiveMessageFromDevice(intent)
+    }
+
+    private fun receiveMessageFromDevice(intent: Intent) {
+        val action = intent.action
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+            val parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)!!
+            with(parcelables) {
+                val inNdefMessage = this[0] as NdefMessage
+                val inNdefRecords = inNdefMessage.records
+                val ndefRecord_0 = inNdefRecords[0]
+
+                val inMessage = String(ndefRecord_0.payload)
+                Log.e("inMessage", inMessage)
+            }
+        }
+    }
+
+    private fun enableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
+
+        val intent = Intent(activity.applicationContext, activity.javaClass)
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+
+        val pendingIntent = PendingIntent.getActivity(activity.applicationContext, 0, intent, 0)
+
+        val filters = arrayOfNulls<IntentFilter>(1)
+        val techList = arrayOf<Array<String>>()
+
+        filters[0] = IntentFilter()
+        with(filters[0]) {
+            this?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
+            this?.addCategory(Intent.CATEGORY_DEFAULT)
+            try {
+                this?.addDataType("application/com.feup.cmov.acme_client")
+            } catch (ex: IntentFilter.MalformedMimeTypeException) {
+                throw RuntimeException("Check your MIME type")
+            }
+        }
+
+        adapter?.enableForegroundDispatch(activity, pendingIntent, filters, techList)
+    }
+
+    private fun disableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
+        adapter?.disableForegroundDispatch(activity)
     }
 
     companion object {
