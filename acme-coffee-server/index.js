eb95bf4a15003f5c027ac3f0494e1b84bb0e26d5
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-11-21 20:43:07 +0000

    Fetch port from environment variables

--- acme-coffee-server/index.js
+++ acme-coffee-server/index.js
@@ -1,4 +1,7 @@
 const app = require('./app');
 
-console.log('Web server started at port 3001');
-app.listen(3001);
+const port = process.env.PORT || 3001;
+
+app.listen(port);
+
+console.log(`Web server started at port ${port}`);
